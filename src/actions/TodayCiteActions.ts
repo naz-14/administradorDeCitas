342827ab3f26ca5a4aa9d74bed388e595abe5110
Uriel Alvarez Valdez <azaki14@gmail.com>
2021-04-11 09:37:15 -0500

    added all the logic for show today cites and delete the start create cuz no necessary, changed type of returns

--- src/actions/TodayCiteActions.ts
+++ src/actions/TodayCiteActions.ts
@@ -1,25 +1,16 @@
-import { AppState, CiteObject } from '../types/states'
+import { CiteObject } from '../types/states'
 import { filterAndSortDates } from '../helpers/filterAndSortDates'
 import moment from 'moment'
-import { ADD__TODAY__CITE, SET__TODAY__CITES, TodayActions } from '../types/ActionTypes'
-import { DataForm } from '../components/CitesForm/CitesForm'
-import { ThunkAction } from 'redux-thunk'
-import { db } from '../firebase/firebase'
+import { ADD__TODAY__CITE, AppActions, SET__TODAY__CITES } from '../types/ActionTypes'
 
-export const startCreateTodayCite = (newCiteObj:DataForm): ThunkAction<void, AppState, unknown, TodayActions> => async (dispatch) => {
-  const notesSnap = await db.collection('cites/').add(newCiteObj)
-  const newCite: CiteObject = { ...newCiteObj, id: notesSnap.id }
-  dispatch(createTodayCite(newCite))
-}
-
-export const setTodayCites = (cites:CiteObject[]): TodayActions => {
+export const setTodayCites = (cites:CiteObject[]): AppActions => {
   const todayCites = filterAndSortDates({ day: moment().format('DD-MM-YYYY'), cites: cites })
   return {
     type: SET__TODAY__CITES,
     payload: todayCites
   }
 }
-export const createTodayCite = (newCite:CiteObject): TodayActions => ({
+export const createTodayCite = (newCite:CiteObject): AppActions => ({
   type: ADD__TODAY__CITE,
   payload: newCite
 })
